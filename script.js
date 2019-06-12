2594dc68ad6f633d3789f28e46e06e6adcf0386f
ashpremcode <ashpremecode@gmail.com>
2019-06-12 18:05:23 +1000

    finished

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate (){
 	const now = new Date();
-	console.log(now);
+
 	const seconds = now.getSeconds();
-	console.log(seconds);
 	const secondDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 	console.log(secondDegrees);
-	secondHand.style.transfrom = `rotate(${secondDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = ((mins / 60) * 360)+ 90;
+	minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+	console.log(minsDegrees);
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360)+ 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	console.log(minsDegrees);
+
+
 }
 
 setInterval(setDate, 1000);
